41d5a45589cfacf7b5aaf09ccf7f3647c8aadf99
Collin <collinhodgs@gmail.com>
2020-08-15 17:27:41 -0600

    added employee table to schema

--- schema.sql
+++ schema.sql
@@ -19,3 +19,13 @@ CREATE TABLE role (
   PRIMARY KEY (id)
   CONSTRAINT FK_departmentId FOREIGN KEY(department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT UNSIGNED,
+  PRIMARY KEY (id)
+  CONSTRAINT FK_managerId FOREIGN KEY(manager_id) REFERENCES employee(id)
+);
