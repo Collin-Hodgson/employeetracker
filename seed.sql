b27d9f7546115db2c30773ce10f8a29ee1a19150
Collin <collinhodgs@gmail.com>
2020-08-15 19:18:50 -0600

    added roles to seed.sql

--- seed.sql
+++ seed.sql
@@ -16,4 +16,22 @@ VALUES ("Finance");
 
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Lead Engineer", )
+VALUES ("Lead Engineer", 150000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales person", 80000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 4);
