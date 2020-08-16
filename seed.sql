a566679404360058222b66e6cdb28b63344acbab
Collin <collinhodgs@gmail.com>
2020-08-15 20:12:43 -0600

    finished seed.sql

--- seed.sql
+++ seed.sql
@@ -35,3 +35,28 @@ VALUES ("Lawyer", 190000, 3);
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Accountant", 125000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Chan", 2, 1)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ashley", "Rodriguez", 3, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Tupik", 4, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Malia", "Brown", 5, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sarah", "Lourd", 6, NULL);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Allen", 7, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Christian", "Eckenrode", 8, 2);
